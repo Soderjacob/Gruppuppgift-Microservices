17b2b8d62f3a95cb9bd21a6f136116b67392b1be
Soderjacob <3tomu24.sodeja@folkuniversitetet.nu>
2025-04-04 21:02:00 +0200

    Inlämning fixad sql script

--- sql_script/database_create.sql
+++ sql_script/database_create.sql
@@ -1,46 +1,39 @@
+-- Skapande av reviews_db
+DROP TABLE IF EXISTS `movie`;
 
--- Skapa databas och populera med en testanvändare för user_service
-CREATE DATABASE user_service;
-
-USE user_service;
-
-CREATE TABLE user_service (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    genre VARCHAR(255) DEFAULT NULL,
-    reviews TEXT DEFAULT NULL,
-    username VARCHAR(255) DEFAULT NULL
+CREATE TABLE `movie` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `genre` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-INSERT INTO user_service (id, genre, reviews, username) VALUES
-(1, NULL, NULL, NULL),
-(2, 'Comedy, action and thriller', NULL, 'Jacob');
+DROP TABLE IF EXISTS `review`;
 
-SELECT * FROM user;
-
--- Skapa databas och populera med en testanvändare för reviews_db
-CREATE DATABASE reviews_db;
-
-USE reviews_db;
-
-CREATE TABLE movie (
-    movie_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    genre VARCHAR(255),
-    release_date DATE
+CREATE TABLE `review` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) DEFAULT NULL,
+  `movie_id` bigint DEFAULT NULL,
+  `rating` int NOT NULL,
+  `user_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-CREATE TABLE review (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT NOT NULL,
-    review_text TEXT,
-    rating INT CHECK (rating BETWEEN 1 AND 5),
-    review_date DATE,
-    FOREIGN KEY (movie_id) REFERENCES movie(movie_id)
+INSERT INTO `review` VALUES 
+(1,'This movie was awesome!',1,5,1),
+(2,'This movie was bad!',2,2,2),
+(3,'This movie was okay.',2,3,3);
+
+-- Skapande av user_service
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `genre` varchar(255) DEFAULT NULL,
+  `reviews` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-INSERT INTO reviews (id, comment, movie_id, rating, user_id) VALUES
-(1, 'This movie was awesome!', 1, 5, 1),
-(2, 'This movie was bad!', 2, 2, 2),
-(3, 'This movie was okay.', 2, 3, 3);
-
-SELECT * FROM review;
+INSERT INTO `user` VALUES (1,NULL,NULL,NULL),
+(2,'Comedy, action and thriller','','Jacob');
